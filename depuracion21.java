5d701783044dd670cb602e4959dfea34217a2589
Nelson <rodriguezdepaznelson@gmail.com>
2024-02-24 18:58:09 +0000

    Codigo con errores

---
+++ depuracion21.java
@@ -0,0 +1,17 @@
+package nrdpPracticadepuracion2;
+
+public class depuracion21 {
+    public static void main(String[] args) {
+        String cadena="La lluvia en Sevilla es una maravilla";
+        int contador=0;
+        for (int i=1;i<cadena.length();i++){
+        //Comprobamos si el caracter es una vocal
+        if(cadena.charAt(i)='a' || cadena.charAt(i)='e' || cadena.charAt(i)='i' || cadena.charAt(i)='o'
+        || cadena.charAt(i)='u'){
+        System.out.println("Encontramos una vocal”);
+        }
+        contador++;
+        }
+        System.out.println("Hay "+contador+" vocales");
+        }
+}
---
+++ depuracion23.java
@@ -0,0 +1,25 @@
+package nrdpPracticadepuracion2;
+
+import java.util.Scanner;
+
+public class depuracion23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el número de ventas");
+        int numVentas=sc.nextInt();
+        /*Declaramos una variable para sumar las ventas,
+        * si lo declaramos dentro del bucle, no podriamos
+        * acceder fuera del bucle a la suma de las ventas.
+        * Esto es por el ámbito.
+        */
+        int sumaVentas=0;
+        for (int i=1;i<numVentas-1;i++){
+        //indico el numero de venta
+        System.out.println("Introduce el precio de la venta "+(i+1));
+        int venta=sc.nextInt();
+        //Acumulamos el valor de la venta
+        sumaVentas=venta;
+        }
+        System.out.println(sumaVentas);
+        }
+}
